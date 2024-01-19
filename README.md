5457733c411f8fbdee4cc21d6e03cb16b056e9f2
nmetal05 <nmetal05@gmail.com>
2024-01-19 23:47:42 +0100

    Initial and final commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# URL Shortener Microservice
+# FreeCodeCamp URL Shortener Microservice Project
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+Welcome to the FreeCodeCamp URL Shortener Microservice project,demonstrating how to use Express.js in full-stack JavaScript development to build an API that can handle POST and GET requests.The goal is to create a service that shortens URLs and redirects users to the original URL when they visit the shortened link.I also used <i>ejs</i> to render a table view that stores the 10 latest urls that were shortened by the service.
+
+# Live Demo
+Experience the efficiency and simplicity of this service by trying the live deployement on :
+🔗
+Seize the opportunity to delve into the intricate details and unravel the inner workings of this exceptional microservice! Explore its functionalities, witness the seamless processes, and gain a comprehensive understanding of how this URL Shortener Microservice works.
+
--- index.js
+++ index.js
@@ -2,22 +2,60 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const ejs = require('ejs')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+let urlDB = []
+// set up ejs
+app.set('view engine','ejs')
+
+app.use(express.urlencoded({extended:true}))
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.use((req,res,next)=>{
+  res.locals.scripts=['/public/common.js']
+  next()
+})
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+/*app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});*/
+
+app.get('/',function(req,res){
+  const origin = req.protocol + '://' + req.get('host');
+  res.render('index',{urlDB,origin})
+})
+
+
+ //define the database as an array to store the short url keys
+let dbIndex = 1
+const urlRegex = /^(http|https):\/\/[^\s/$.?#].[^\s]*$/ //regex for basic url validation
+//url shortening microservice
+app.post('/api/shorturl/',(req,res)=>{
+  let url = req.body.url
+  if(urlRegex.test(url)){
+    let urlKey = dbIndex++
+    urlDB.push({urlKey,url})
+    res.json({original_url:url,short_url:urlKey})
+  }else{
+    res.json({error:"Invalid URL"})
+  }
+ })
+
+//urlKey redirection
+app.get('/api/shorturl/:link',(req,res)=>{
+  let link = parseInt(req.params.link)
+  let urlPlace = urlDB.find(item => item.urlKey==link)
+  if(urlPlace){
+    res.redirect(urlPlace.url)
+  }else{
+    res.json({error:"No short URL for the given index key"})
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
